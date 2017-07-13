84d587aaacb3e1c4ec5fb2030153f52f9fb4888a
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-07-13 16:58:53 -0600

    :sparkles: add test cases

--- fibonacci.py
+++ fibonacci.py
@@ -3,9 +3,10 @@ def fib(n):
         return 1
     return fib(n-1) + fib(n-2)
 
-print fib(0)
-print fib(1)
-print fib(2)
-print fib(3)
-print fib(4)
-print fib(5)
+assert fib(0)==1
+assert fib(1)==1
+assert fib(2)==2
+assert fib(3)==3
+assert fib(4)==5
+assert fib(5)==8
+assert fib(25)==121393
